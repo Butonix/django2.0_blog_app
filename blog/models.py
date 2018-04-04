09661fa4749d3f8120a408b30fd4e6a45c8d4a27
Simon <simonmarkholland@gmail.com>
2018-04-04 14:51:25 +0700

    Fix typoes in models

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete='CASCADE')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now())
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
@@ -18,16 +18,16 @@ class Post(models.Model):
         return self.comments.filter(approved_comment=True)
 
     def get_absolute_url(self):
-        return reverse("post_detail", kwargs={'pk:self.pk'})
+        return reverse("post_detail", kwargs={'pk':self.pk})
 
     def __str__(self):
         return self.title
 
-class Commment(models.Model):
+class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments', on_delete='CASCADE')
     author = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now())
     approved_comment = models.BooleanField(default=False)
 
     def approve(self):
